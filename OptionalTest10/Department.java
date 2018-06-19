52e28defaa6bdbe283b791fb4d152bfb51158eb4
thuydt9 <thuydt.it1810@gmail.com>
2018-06-19 23:58:41 +0700

    Commit Day5

---
+++ OptionalTest10/Department.java
@@ -0,0 +1,40 @@
+package OptionalTest10;
+
+import java.util.Scanner;
+
+public class Department {
+     String idD;
+    String nameD;
+    String descriptionD;
+
+    public Department(String idD, String nameD, String descriptionD) {
+    }
+
+    public Department() {
+
+    }
+
+    public String getIdD() {
+            return idD;
+        }
+
+        public void setIdD(String idD) {
+            this.idD = idD;
+        }
+
+        public String getNameD() {
+            return nameD;
+        }
+
+        public void setNameD(String nameD) {
+            this.nameD = nameD;
+        }
+
+        public String getDescriptionD() {
+            return descriptionD;
+        }
+
+        public void setDescriptionD(String descriptionD) {
+            this.descriptionD = descriptionD;
+        }
+    }
